2e17cca326f2e6bc910d1278cdf1f6517190e73b
Lauri Myllari <lauri.myllari@gmail.com>
2017-07-19 10:32:22 -0700

    Add 64bit Windows symbols

--- fetch.sh
+++ fetch.sh
@@ -15,16 +15,18 @@ fi
 for VERSION in "$VERSIONS"
 do
 
-	mkdir -p symbols/{win32-$VERSION,darwin-$VERSION,linux-$VERSION}
+	mkdir -p symbols/{win32-$VERSION,win32-x64-$VERSION,darwin-$VERSION,linux-$VERSION}
 
 	if [ `which bsdtar` ] && [ ! $FORCE_UNZIP ]; then 
 		cmd="bsdtar -xvf-"
 		( cd symbols/win32-$VERSION ; wget -O - https://github.com/atom/electron/releases/download/v${VERSION}/electron-v${VERSION}-win32-ia32-symbols.zip | $cmd )
+		( cd symbols/win32-x64-$VERSION ; wget -O - https://github.com/atom/electron/releases/download/v${VERSION}/electron-v${VERSION}-win32-x64-symbols.zip | $cmd )
 		( cd symbols/darwin-$VERSION ; wget -O - https://github.com/atom/electron/releases/download/v${VERSION}/electron-v${VERSION}-darwin-x64-symbols.zip | $cmd )
 		( cd symbols/linux-$VERSION; wget -O - https://github.com/atom/electron/releases/download/v${VERSION}/electron-v${VERSION}-linux-x64-symbols.zip | $cmd )
 	elif [ `which unzip` ]; then
 		cmd="unzip -x"
 		( cd symbols/win32-$VERSION ; wget -O - https://github.com/atom/electron/releases/download/v${VERSION}/electron-v${VERSION}-win32-ia32-symbols.zip > /tmp/win32.zip ; $cmd /tmp/win32.zip )
+		( cd symbols/win32-x64-$VERSION ; wget -O - https://github.com/atom/electron/releases/download/v${VERSION}/electron-v${VERSION}-win32-x64-symbols.zip > /tmp/win32-x64.zip ; $cmd /tmp/win32-x64.zip )
 		( cd symbols/darwin-$VERSION ; wget -O - https://github.com/atom/electron/releases/download/v${VERSION}/electron-v${VERSION}-darwin-x64-symbols.zip > /tmp/darwin.zip ; $cmd /tmp/darwin.zip )
 		( cd symbols/linux-$VERSION ; wget -O - https://github.com/atom/electron/releases/download/v${VERSION}/electron-v${VERSION}-linux-x64-symbols.zip > /tmp/linux.zip ; $cmd /tmp/linux.zip )	
 	fi
--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var path = require("path");
 var fs = require("fs");
 
-var paths = [__dirname+"/symbols/win32/electron.breakpad.syms", __dirname+"/symbols/darwin/electron.breakpad.syms", __dirname+"/symbols/linux/electron.breakpad.syms"];
+var paths = [__dirname+"/symbols/win32/electron.breakpad.syms", __dirname+"/symbols/win32-x64/electron.breakpad.syms", __dirname+"/symbols/darwin/electron.breakpad.syms", __dirname+"/symbols/linux/electron.breakpad.syms"];
 try { 
 	paths = fs.readdirSync(__dirname+"/symbols").map(function(x) { return path.join(__dirname+"/symbols", x) });
 } catch(e) { }
 
 module.exports = { paths: paths, pathsForVer: function(ver) { 
-	return [__dirname+"/symbols/win32-"+ver+"/electron.breakpad.syms", __dirname+"/symbols/darwin-"+ver+"/electron.breakpad.syms", __dirname+"/symbols/linux-"+ver+"/electron.breakpad.syms"]
+	return [__dirname+"/symbols/win32-"+ver+"/electron.breakpad.syms", __dirname+"/symbols/win32-x64-"+ver+"/electron.breakpad.syms", __dirname+"/symbols/darwin-"+ver+"/electron.breakpad.syms", __dirname+"/symbols/linux-"+ver+"/electron.breakpad.syms"]
 } };
